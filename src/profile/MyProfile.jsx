e5557958b1c5d66ea38aa707cfcf6507394852da
mdfaisal9045 <mdfaisal9045@gmail.com>
2025-06-20 18:10:47 +0530

    updated

--- src/profile/MyProfile.jsx
+++ src/profile/MyProfile.jsx
@@ -1,20 +1,7 @@
-// import React from 'react'
-
-// const MyProfile = (props) => {
-//   console.log(props);
-  
-//   return (
-//     <div>MyProfile</div>
-//   )
-// }
-
-// export default MyProfile
-
-
 import React from 'react'
 
-const MyProfile = ({information}) => {
-  console.log(information);
+const MyProfile = (props) => {
+  console.log(props);
   
   return (
     <div>MyProfile</div>
@@ -22,3 +9,16 @@ const MyProfile = ({information}) => {
 }
 
 export default MyProfile
+
+
+// import React from 'react'
+
+// const MyProfile = ({information}) => {
+//   console.log(information);
+  
+//   return (
+//     <div>MyProfile</div>
+//   )
+// }
+
+// export default MyProfile
